bc7c4849ec2b5bbbfaec5394ed1f34f21b7e680b
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-11-06 17:50:25 -0500

    feat: envs were setted in typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { databaseConfig } from './typeorm.config';
 import { UsersModule } from './users/users.module';
 import { CoffeeHouseModule } from './coffee-house/coffee-house.module';
 import { AuthsModule } from './auths/auths.module';
 import { S3Module } from './s3/s3.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 
 @Module({
@@ -14,7 +13,22 @@ import configuration from './config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
-    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('db.host'),
+          port: configService.get('db.port'),
+          username: configService.get('db.username'),
+          password: configService.get('db.password'),
+          database: 'postgres',
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
     UsersModule,
     CoffeeHouseModule,
     AuthsModule,
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,4 +4,11 @@ export default () => ({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
   },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
 });
